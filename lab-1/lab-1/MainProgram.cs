c3cd67839beaa4684de745fdc3fccc2dd55365fa
Utkin Ilia <utkinilia.fr@gmail.com>
2018-02-19 21:39:04 +0600

    lab_1 v1.0

---
+++ lab-1/lab-1/MainProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            SeparatingDigits stringNumbers = new SeparatingDigits();
+            Console.WriteLine("Введите шестизначное число: ");
+            int number = int.Parse(Console.ReadLine());
+            string result = stringNumbers.separation(number);
+            Console.WriteLine($"Сумма первых 3 десятичных цифр {result} сумме 3 последних десятичных цифр");
+            Console.Read();
+        }
+    }
+}
---
+++ lab-1/lab-1/SeparatingNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace lab1
+{
+    class SeparatingDigits
+    {
+        public string separation(int number)
+        {
+            string result = "не равна";
+            int firstDigit = number / 100000,
+                secondDigit = (number / 10000) % 10,
+                thirdDigit = (number / 1000) % 10,
+                forthDigit = (number / 100) % 10,
+                fifthDigit = (number / 10) % 10,
+                sixthDigit = number % 10;
+            if ((firstDigit + secondDigit + thirdDigit) == (forthDigit + fifthDigit + sixthDigit))
+            {
+                result = "равна";
+            }
+            return result;
+        }
+    }
+}
