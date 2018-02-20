da63573d07500843b66bbbebf9d692173ee31f24
Utkin Ilia <utkinilia.fr@gmail.com>
2018-02-20 09:56:00 +0600

    lab_1 v1.1

--- lab-1/lab-1/SeparatingNumbers.cs
+++ lab-1/lab-1/SeparatingNumbers.cs
@@ -5,12 +5,12 @@
         public string separation(int number)
         {
             string result = "не равна";
-            int firstDigit = number / 100000,
-                secondDigit = (number / 10000) % 10,
-                thirdDigit = (number / 1000) % 10,
-                forthDigit = (number / 100) % 10,
-                fifthDigit = (number / 10) % 10,
-                sixthDigit = number % 10;
+            int firstDigit = number / 100000;
+            int secondDigit = (number / 10000) % 10;
+            int thirdDigit = (number / 1000) % 10;
+            int forthDigit = (number / 100) % 10;
+            int fifthDigit = (number / 10) % 10;
+            int sixthDigit = number % 10;
             if ((firstDigit + secondDigit + thirdDigit) == (forthDigit + fifthDigit + sixthDigit))
             {
                 result = "равна";
