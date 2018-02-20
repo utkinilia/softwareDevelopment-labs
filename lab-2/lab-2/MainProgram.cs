0cffc3f0844e90627f2aee7c327ec021e5805603
Utkin Ilia <utkinilia.fr@gmail.com>
2018-02-20 10:34:20 +0600

    lab_2 v1.0

---
+++ lab-2/lab-2/MainProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab_2
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            RemovingSpaces a = new RemovingSpaces();
+            Console.WriteLine("Введите строку: ");
+            string text = Console.ReadLine();
+            if (String.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Ошибка! Вы не ввели текст");
+            }
+            else
+            {
+                string result = RemovingSpaces.Delete(text);
+                Console.WriteLine("\nРезультат: ");
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab-2/lab-2/RemovingSpaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab_2
+{
+    class RemovingSpaces
+    {
+        public static string Delete(string text)
+        {
+            int i = 0;
+            do
+            {
+                if (text[i] == ' ' && text[i + 1] == ' ')
+                    text = text.Remove(i, 1);
+                else
+                    i++;
+            }
+            while (i < text.Length);
+            return text;
+        }
+    }
+}
