8f32e7ead862454165071994c98d9ad4bec15ed5
utkinilia <utkinilia.fr@gmail.com>
2018-03-06 10:30:31 +0400

    lab_4 v1.0

---
+++ lab-4/lab-4/MainProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_4
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            SwapNumbers obj = new SwapNumbers();
+            int[] array = new int[] { 4, 5, 6, 7, -12, -3, 4, -5, -7, 2 };
+
+            Console.Write("Исходный массив: \n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            SwapNumbers.swap(array);
+
+            Console.Write("\n\nРезультат: \n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab-4/lab-4/SwapNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab_4
+{
+    class SwapNumbers
+    {
+        static public int[] swap(int[] array)
+        {
+            int positiveElement = array[0];
+            int negativeElement = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0 && array[i] < positiveElement)
+                {
+                    positiveElement = array[i];
+                }
+                else if (array[i] < 0 && array[i] < negativeElement)
+                {
+                    negativeElement = array[i];
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == positiveElement)
+                {
+                    array[i] = negativeElement;
+                }
+                else if (array[i] == negativeElement)
+                {
+                    array[i] = positiveElement;
+                }
+            }
+            return array;
+        }
+    }
+}
