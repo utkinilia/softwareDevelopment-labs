753e08843ed16adff3fc61cc22a47f202d6ee92b
utkinilia <utkinilia.fr@gmail.com>
2018-03-06 10:29:46 +0400

    lab_3 v1.0

---
+++ lab-3/lab-3/MainProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_3
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            SearchArray obj = new SearchArray();
+            List<int> array = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                int element = int.Parse(Console.ReadLine());
+                array.Add(element);
+            }
+
+            array = obj.searchSameElements(array);
+
+            Console.WriteLine("\nРезультат: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab-3/lab-3/SearchArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_3
+{
+    class SearchArray
+    {
+        public List<int> searchSameElements(List<int> array)
+        {
+            List<int> result = new List<int>();
+            int count = 1;
+            int number = array[0];
+            for (int i = 1; i < array.Count; i++)
+            {
+                if (array[i] == number)
+                    count++;
+                else
+                {
+                    result.Add(count);
+                    count = 1;
+                    number = array[i];
+                }
+            }
+            result.Add(count);
+            return result;
+        }
+    }
+}
