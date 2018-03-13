b4eb6aa0d2d37d251f912582c0e588be79287cf3
utkinilia <utkinilia.fr@gmail.com>
2018-03-13 10:24:16 +0400

    lab-5 v1.0

---
+++ lab-5/lab-5/MainProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_5
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            AddZero addZero = new AddZero();
+            List<double> array = new List<double>();
+            Console.WriteLine("Введите число элементов массива: ");
+            int numbersElements = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbersElements; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                double element = double.Parse(Console.ReadLine());
+                array.Add(element);
+            }
+
+            array = addZero.addZero(array);
+
+            Console.WriteLine("Результат: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
