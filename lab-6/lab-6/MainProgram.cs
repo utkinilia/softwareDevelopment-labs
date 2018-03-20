4cbb16a7eaa7dd2ebe89e9ca485f65b06e048803
utkinilia <utkinilia.fr@gmail.com>
2018-03-20 10:53:59 +0400

    lab_6 v1.0

---
+++ lab-6/lab-6/MainProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveFromArray removeFromArray = new RemoveFromArray();
+            List<double> array = new List<double>();
+
+            Console.WriteLine("Введите количество элементов: ");
+            int amountOfElements = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amountOfElements; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                double element = double.Parse(Console.ReadLine());
+                array.Add(element);
+            }
+
+            array = removeFromArray.DeleteElement(array);
+
+            Console.WriteLine("Результат: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ lab-6/lab-6/RemoveFromArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace lab_6
+{
+    public class RemoveFromArray
+    {
+        public List<double> DeleteElement(List<double> array)
+        {
+            double average = ArithmeticMean(array);
+
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                if (array[i] > average)
+                {
+                    array.Remove(array[i]);
+                }
+            }
+
+            return array;
+        }
+
+        private double ArithmeticMean(List<double> array)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                sum += array[i];
+            }
+
+            double average = sum / array.Count;
+            return average;
+        }
+    }
+}
