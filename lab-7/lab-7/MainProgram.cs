8ef86127ad32c07a808f2f172423d3f28d050131
utkinilia <utkinilia.fr@gmail.com>
2018-03-27 09:49:51 +0400

    lab-7 v1.0

---
+++ lab-7/lab-7/MainProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace lab_7
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Matrix mtx = new Matrix();
+            int[,] matrix = new int[5, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = random.Next(-9, 9);
+                }
+            }
+            Console.WriteLine("Исходная матрица: ");
+            mtx.print(matrix);
+
+            matrix = mtx.replacement(matrix);
+
+            Console.WriteLine("\nИзмененная матрица: ");
+            mtx.print(matrix);
+            Console.Read();
+        }
+    }
+}
---
+++ lab-7/lab-7/Matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab_7
+{
+    public class Matrix
+    {
+        public int[,] replacement(int [,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if ((i < j && matrix[i, j] > 0 && matrix[i, j] % 2 != 0) || (i + j > matrix.GetLength(0) - 1 && matrix[i, j] > 0 && matrix[i, j] % 2 != 0))
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        public int[,] print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(" " + matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return matrix;
+        }
+    }
+}
