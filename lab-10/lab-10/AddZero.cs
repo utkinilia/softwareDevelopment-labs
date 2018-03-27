d0e3ef881cb4a7d6dc8f66daf933e9c3b433a659
utkinilia <utkinilia.fr@gmail.com>
2018-03-27 09:55:09 +0400

    lab_10 v1.0

---
+++ lab-10/lab-10/AddZero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_10
+{
+    class AddZero
+    {
+        public List<double> addZero(List<double> array)
+        {
+            double sum = Math.Abs(array[0]) + Math.Abs(array[1]) + Math.Abs(array[array.Count - 1]);
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (sum % array[i] == 0)
+                {
+                    array.Insert(i + 1, 0);
+                }
+            }
+            return array;
+        }
+    }
+}
+
---
+++ lab-10/lab-10/MainProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace lab_10
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            AddZero addZero = new AddZero();
+            List<double> array = new List<double>();
+            StreamReader read = new StreamReader("input.txt");
+
+            string line;
+
+            while ((line = read.ReadLine()) != null)
+            {
+                array.Add(Convert.ToDouble(line));
+            }
+
+            array = addZero.addZero(array);
+
+            StreamWriter write = new StreamWriter("output.txt");
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                write.Write(array[i] + " ");
+            }
+
+            write.Close();
+
+            Console.Read();
+        }
+    }
+}
