a7de506a1e04c1dec572ee71a5b0b5ad663f4bbc
utkinilia <utkinilia.fr@gmail.com>
2018-03-27 09:54:39 +0400

    lab_8 v1.0

---
+++ lab-8/lab-8/AddRow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace lab_8
+{
+    class AddRow
+    {
+        public List<int> Add(List<List<int>> array)
+        {
+            List<int> newArray = new List<int>();
+            for (int i = 0; i < array.Count; i++)
+            {
+                int sum = Search(i, array);
+                for (int j = 0; j < array[i].Count; j++)
+                {
+                    if (array[i][j] < 0)
+                    {
+                        newArray.Add(sum);
+                    }
+                }
+            }
+            return newArray;
+        }
+
+        private int Search(int i, List<List<int>> array)
+        {
+            int sum = 0;
+            for (int j = 0; j < array[i].Count; j++)
+            {
+                if (array[i][j] > 0)
+                {
+                    sum += array[i][j];
+                }
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ lab-8/lab-8/MainProgram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_8
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> array = new List<List<int>>();
+            array.Add(new List<int>() { 5, 10, -1 });
+            array.Add(new List<int>() { 1, -6, 9 });
+            array.Add(new List<int>() { 4, 6, -3, });
+
+            Console.Write("Исходный массив: \n");
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array[i].Count; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            AddRow addRow = new AddRow();
+
+            array.Add(addRow.Add(array));
+
+            Console.Write("\nРезультат: \n");
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array[i].Count; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
