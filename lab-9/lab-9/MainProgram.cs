5b4bd28dbddc64b2559e5e0162f7c17fb63f554e
utkinilia <utkinilia.fr@gmail.com>
2018-03-27 10:03:59 +0400

    lab_9 v1.0

---
+++ lab-9/lab-9/MainProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab_9
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5,5];
+            Random random = new Random();
+
+            Sorting sortingArray = new Sorting();
+            
+            Console.Write("Исходный массив: \n");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 9);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            array = sortingArray.SortingArray(array);
+
+            Console.Write("\nОтсортированный массив: \n");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab-9/lab-9/Sorting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace lab_9
+{
+    class Sorting
+    {
+        private List<int> oneDimensionalArray = new List<int>();
+
+        public int[,] SortingArray(int[,] array)
+        {
+            Formating(array);
+            int z = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = oneDimensionalArray[z];
+                    z++;
+                }
+            }
+            return array;
+        }
+
+        private List<int> Formating(int[,] array)
+        {
+            int k = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    oneDimensionalArray.Add(array[i, j]);
+                }
+            }
+            oneDimensionalArray.Sort();
+            return oneDimensionalArray;
+        }
+    }
+}
